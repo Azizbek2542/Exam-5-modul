d8291fdfd6b354c41bdd83882fb7743174d79a20
Azizbek2542 <tuhtasinova.x@gmail.com>
2025-07-24 11:53:15 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,94 @@
+// 1 lesson:
+
+// let a = 5;
+// let b = 2;
+// let c = a/b;
+// alert(c);
+
+// 2lesson:
+
+// let random = Math.round(Math.random() * 10 );
+// alert(`Random orqali chiqqan son: ${random}`);
+
+// 3 lesson:
+
+// let a = 12.510;
+// alert(Math.floor(a));
+
+// 4 lesson:
+
+// let text = "MARS IT SCHOOL";
+// let length = text.length;
+
+
+// alert(length);
+
+// 5 lesson:
+
+// for (let index = 0; index < 10; index++) {
+//     console.log("MArsItschool");
+// }
+
+// 6 lesson:
+
+// let harflar = ["a" , "b" , "c", "d" , "e" , "f" , "g" , "h" , "i" , "j" , "k" , "l" , "m" , "n" , "o" , "p" , "q" , "r" , "s" , "t",  "u" , "v" , "x" , "y" , "z" ];
+// let nameindex = [0, 24, 8, 24];
+// let name = '';
+// for (let index = 0; index < nameindex.length; index++) {
+//     name += harflar[nameindex[index]];
+// }
+// console.log(name)
+
+// 7 lesson:
+
+// const age = prompt('Yosingizni kiriting');
+
+// if (age>18) {
+//     console.log("Siz balag'ot yoshiga yetgansiz")
+// }else if (age<18) {
+//     console.log("siz balog'at yoshiga yetmagansiz")
+// }else {
+//     console.log(" balog'at yoshi muborak")
+// };
+
+// 8 lesson:
+
+// const name = prompt("Введите своё имя:");
+// const reversed = name.split('').reverse().join('');
+// console.log("Ваше имя наоборот: " + reversed);
+
+// 9 lesson:
+
+// const userName = prompt("Ismingizni kiriting");
+// const age = prompt("Yoshingizni kiriting");
+
+// const newWord = document.createElement("h1");
+// newWord.textContent = `Ismingiz: ${userName} | Yoshingiz: ${age}`;
+// document.body.appendChild(newWord);
+
+// newWord.style.backgroundColor = "#4CAF50";
+// newWord.style.color = "white";
+// newWord.style.padding = "20px";
+// newWord.style.fontSize = "20px";
+// newWord.style.fontFamily = "Arial, sans-serif";
+// newWord.style.borderRadius = "10px";
+// newWord.style.width = "fit-content";
+// newWord.style.margin = "20px auto";
+// newWord.style.boxShadow = "0 4px 8px rgba(0, 0, 0, 0.2)";
+
+
+// 10 lesson:
+
+// let sonlar = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let juftArray = [];
+
+
+// console.log(sonlar, "boshlanishdagi array")
+
+// for (let i = 0; i < sonlar.length; i++) {
+//   if (sonlar[i] % 2 === 0) {
+//     juftArray.push(sonlar[i]);
+//   }
+// }
+
+// console.log(juftArray, "juft sonlarga ajratilgan array");
